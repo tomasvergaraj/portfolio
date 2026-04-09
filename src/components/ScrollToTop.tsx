8c6f6a5fc11a5a02bbe2e4824773eb7338142085
Tomas Vergara <tomas.d.v.j@hotmail.com>
2026-04-09 17:25:33 -0400

    fix: scroll to top en mobile

--- src/components/ScrollToTop.tsx
+++ src/components/ScrollToTop.tsx
@@ -1,12 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect, useLayoutEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 
+const scrollPageToTop = () => {
+  window.scrollTo({ top: 0, left: 0, behavior: 'auto' });
+  document.documentElement.scrollTop = 0;
+  document.body.scrollTop = 0;
+};
+
 const ScrollToTop = () => {
-  const { pathname } = useLocation();
+  const { pathname, search } = useLocation();
 
   useEffect(() => {
-    window.scrollTo({ top: 0, left: 0 });
-  }, [pathname]);
+    if (!('scrollRestoration' in window.history)) {
+      return undefined;
+    }
+
+    const previousScrollRestoration = window.history.scrollRestoration;
+    window.history.scrollRestoration = 'manual';
+
+    return () => {
+      window.history.scrollRestoration = previousScrollRestoration;
+    };
+  }, []);
+
+  useLayoutEffect(() => {
+    scrollPageToTop();
+
+    const animationFrameId = window.requestAnimationFrame(scrollPageToTop);
+    const timeoutId = window.setTimeout(scrollPageToTop, 80);
+
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+      window.clearTimeout(timeoutId);
+    };
+  }, [pathname, search]);
 
   return null;
 };
